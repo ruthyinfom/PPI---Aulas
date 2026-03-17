810f1df7c0768f2254c8128642d8eddcab9f936f
ruthyinfom <ruthyanne.priscila@academico.ifrn.edu.br>
2026-03-17 15:02:38 -0300

    slk

---
+++ Typescript Aulas/ex1.ts
@@ -0,0 +1,11 @@
+let nome = 'Ruthy'
+let matr = 20231144010014
+let outra: string = 'Ágata'
+console.log(outra)
+let pessoa: any //qualquer tipo 
+
+//undefined: sem valor definido 
+let bim1: number | undefined 
+console.log(bim1)
+bim1 = 75
+console.log(bim1)
