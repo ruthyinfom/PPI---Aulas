c23b68168a76bb6fafda0e94641fa086d7963bec
ruthyinfom <ruthyanne.priscila@academico.ifrn.edu.br>
2026-04-16 23:51:23 -0300

    atividade

--- poo/emprestimo.js
+++ poo/emprestimo.js
@@ -1,21 +1,71 @@
-var Emprestimo = /** @class */ (function () {
-    function Emprestimo(li, ps) {
+"use strict";
+class Emprestimo {
+    livro; //Livro 
+    usuario; //pessoa 
+    dataEmprestimo;
+    dataPrevistaDevolucao;
+    constructor(li, ps) {
         this.livro = li;
-        this.usuário = ps;
+        this.usuario = ps;
         this.dataEmprestimo = new Date();
-        this.dataPrevistaDevolução = new Date();
-        this.dataPrevistaDevolução.setDate(this.dataEmprestimo.getDate() + 15);
+        this.dataPrevistaDevolucao = new Date();
+        this.dataPrevistaDevolucao.setDate(this.dataEmprestimo.getDate() + 15);
     }
-    return Emprestimo;
-}());
-var emp1 = new Emprestimo('Memórias Póstumas de Bras Cubas', 'Bruno');
-console.log(emp1);
-/* Exercício:
-    1. Fazer uma classe pessoa com os atributos nome e cpf
-    2. Fazer uma classe Exemplar com os atributos:
-            - isbn (código que identifica uma publicação)
-            - título
-            - autor e ano
-            - ano
-    3. Modificar a classe empréstimo: tipar com Exemplar e pessoa
-*/ 
+    obterDataEmprestimo() {
+        return this.dataEmprestimo.toLocaleDateString();
+    }
+    obterDataPrevistaDevolucao() {
+        return this.dataPrevistaDevolucao.toLocaleDateString();
+    }
+}
+class Pessoa {
+    nome;
+    cpf;
+    constructor(nm, cp) {
+        this.nome = nm;
+        this.cpf = cp;
+    }
+    obterNome() {
+        return this.nome;
+    }
+    obterCpf() {
+        return this.cpf;
+    }
+}
+class Exemplar {
+    isbn;
+    titulo;
+    autor;
+    ano;
+    constructor(is, ti, at, an) {
+        this.isbn = is;
+        this.titulo = ti;
+        this.autor = at;
+        this.ano = an;
+    }
+    obterIsbn() {
+        return this.isbn;
+    }
+    obterTitulo() {
+        return this.titulo;
+    }
+    obterAutor() {
+        return this.autor;
+    }
+    obterAno() {
+        return this.ano;
+    }
+}
+let pessoa1 = new Pessoa("Ruthyanne", "123.456.789-00");
+let livro1 = new Exemplar(1234567890, "Memórias póstumas de Brás Cubas", "Machado de Assis", 1881);
+let emp1 = new Emprestimo(livro1, pessoa1);
+console.log("-------------");
+console.log(`Usuário: ${pessoa1.obterNome()} - CPF: ${pessoa1.obterCpf()}`);
+console.log("-------------");
+console.log(`Livro: ${livro1.obterTitulo()}
+Autor: ${livro1.obterAutor()}
+Ano: ${livro1.obterAno()}
+ISBN: ${livro1.obterIsbn()}`);
+console.log("-------------");
+console.log("Data do empréstimo:", emp1.obterDataEmprestimo());
+console.log("Data de devolução:", emp1.obterDataPrevistaDevolucao());
--- poo/emprestimo.ts
+++ poo/emprestimo.ts
@@ -1,11 +1,11 @@
 class Emprestimo {
-    livro: string //Livro 
-    usuario: string //pessoa 
+    livro: Exemplar //Livro 
+    usuario: Pessoa //pessoa 
     dataEmprestimo: Date
     dataPrevistaDevolucao : Date
 
-    constructor(li: string, ps: string) {
-        this.livro = li
+    constructor(li: Exemplar, ps: Pessoa) {
+        this.livro = li 
         this.usuario = ps
         this.dataEmprestimo = new Date()
         this.dataPrevistaDevolucao = new Date()
@@ -22,23 +22,60 @@ class Emprestimo {
         return this.dataPrevistaDevolucao.toLocaleDateString() 
     }
 }
+
 class Pessoa {
-    nome: 
-    cpf: 
+    nome: string; 
+    cpf: string 
+
+    constructor (nm: string, cp: string) {
+        this.nome = nm
+        this.cpf = cp 
 }
+    obterNome(): string {
+        return this.nome
+}
+    obterCpf(): string {
+        return this.cpf
+}
+}
+
 class Exemplar {
+    isbn: number;
+    titulo: string; 
+    autor: string; 
+    ano: number
 
+    constructor (is: number, ti: string, at: string, an: number) {
+        this.isbn = is
+        this.titulo = ti
+        this.autor = at
+        this.ano = an 
+}
+    obterIsbn(): number {
+        return this.isbn 
+}
+    obterTitulo(): string {
+        return this.titulo
+}
+    obterAutor(): string {
+        return this.autor
+}
+    obterAno(): number {
+        return this.ano 
+}
 }
 
-let emp1 = new Emprestimo('Memórias Póstumas de Bras Cubas', 'Bruno')
-console.log(emp1)
+let pessoa1 = new Pessoa ("Ruthyanne", "123.456.789-00")
+let livro1 = new Exemplar(1234567890, "Memórias póstumas de Brás Cubas", "Machado de Assis", 1881)
+let emp1 = new Emprestimo(livro1, pessoa1)
 
-/* Exercício: 
-    1. Fazer uma classe pessoa com os atributos nome e cpf 
-    2. Fazer uma classe Exemplar com os atributos: 
-            - isbn (código que identifica uma publicação)
-            - título 
-            - autor e ano 
-            - ano 
-    3. Modificar a classe empréstimo: tipar com Exemplar e pessoa
-*/ 
+console.log("-------------")
+console.log(`Usuário: ${pessoa1.obterNome()} - CPF: ${pessoa1.obterCpf()}`)
+console.log("-------------")
+console.log(`Livro: ${livro1.obterTitulo()}
+Autor: ${livro1.obterAutor()}
+Ano: ${livro1.obterAno()}
+ISBN: ${livro1.obterIsbn()}`)
+console.log("-------------")
+console.log("Data do empréstimo:", emp1.obterDataEmprestimo())
+console.log("Data de devolução:", emp1.obterDataPrevistaDevolucao())
